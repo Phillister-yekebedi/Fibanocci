387e204d07a85fa46990c75d2d7a87e90ae352c1
Phillister-yekebedi <otienoyekebedi@gmail.com>
2023-05-25 15:29:08 +0300

    first commit

--- app/src/main/java/com/example/fibonacci/FibonacciRvAdapter.kt
+++ app/src/main/java/com/example/fibonacci/FibonacciRvAdapter.kt
@@ -1,4 +1,23 @@
 package com.example.fibonacci
 
-class FibonacciRvAdapter {
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class NumbersRvAdapter(var numbers:List<Int>):RecyclerView.Adapter<NumbersRvAdapter.NumbersViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumbersRvAdapter.NumbersViewHolder {
+        val view =LayoutInflater.from(parent.context).inflate(R.layout.number_list_item,parent,false)
+        return NumbersViewHolder(view)
+    }
+    override fun onBindViewHolder(holder: NumbersRvAdapter.NumbersViewHolder, position: Int) {
+        holder.tvNumbers.text = numbers[position].toString()
+    }
+
+    override fun getItemCount(): Int = numbers.size
+
+    class NumbersViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        var tvNumber = itemView.findViewById<TextView>(R.id.tvNumbers)
+
+    }
 }
--- app/src/main/java/com/example/fibonacci/MainActivity.kt
+++ app/src/main/java/com/example/fibonacci/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.fibonacci
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.fibonacci.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        fibonacciNumb()
     }
 }
